e0a0a3ff0e9e4d2851f34532ddc0c9b9981cf04b
Sasha Egorov <a.a.egoroff@gmail.com>
2016-09-27 17:54:57 +0400

    More pug-lint rules

--- .pug-lintrc.js
+++ .pug-lintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
+  disallowBlockExpansion: true,
   disallowClassAttributeWithStaticValue: true,
-  disallowClassLiteralsBeforeIdLiterals: true
+  disallowClassAttributeWithStaticValue: true,
+  disallowClassLiteralsBeforeIdLiterals: true,
+  disallowDuplicateAttributes: true,
+  disallowHtmlText: true,
+  disallowIdAttributeWithStaticValue: true,
+  disallowLegacyMixinCall: true,
+  disallowMultipleLineBreaks: true,
+  disallowSpacesInsideAttributeBrackets: true,
+  disallowTemplateString: true,
+
+  requireClassLiteralsBeforeAttributes: true,
+  requireIdLiteralsBeforeAttributes: true,
+  requireLineFeedAtFileEnd: true,
+  requireLowerCaseAttributes: true,
+  requireLowerCaseTags: true,
+  requireSpaceAfterCodeOperator: true,
+  requireStrictEqualityOperators: true,
+
+  validateAttributeQuoteMarks: "'",
+  validateAttributeSeparator: {
+    "separator": " ", "multiLineSeparator": "\n  "
+  },
+  validateDivTags: true,
+  validateExtensions: true,
+  validateIndentation: 2,
+  validateLineBreaks: 'LF',
+  validateSelfClosingTags: true,
+  validateTemplateString: ['concatenation']
 };
