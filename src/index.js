1c865bdf1159beb6a32909feb3324f9f6adda0a4
Sasha Egorov <a.a.egoroff@gmail.com>
2016-10-30 16:48:25 +0400

    SCSS added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const hello = require('./hello.js')
+require('./main.scss')
 
 console.log('index.js works')
 hello()
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const absolutePath = path.join(__dirname, 'build')
 
 module.exports = {
+  devtool: 'source-map',
+
   entry: {
     index: './src/index.js'
   },
 
-  devtool: 'source-map',
-
   output: {
     filename: '[name].js',
     path: absolutePath
+    // Has issues with `webpack-dev-server`
+    // publicPath: assetPath
   },
 
   module: {
@@ -29,6 +32,23 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loaders: ['babel-loader?presets=es2015']
+      },
+
+      // Fonts and SVG
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+
+      // Foundation
+      {
+        test: /\.scss$/,
+        exclude: [/node_modules/], // sassLoader will include node_modules explicitly
+        // Using ExtractTextPlugin:
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass?sourceMap&outputStyle=expanded')
+        // Using style-loader:
+        // loaders: ['style', 'css', 'sass']
       }
     ],
 
@@ -47,13 +67,17 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({  // Also generate a test.html
-      filename: './index.html',
+      filename: '../index.html',
       template: './src/index.pug',
       inject: 'body',
       xhtml: true
     }),
+
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
-    })
+    }),
+
+    // TODO: Move to production settings
+    new ExtractTextPlugin('styles', 'main.css')
   ]
 }
